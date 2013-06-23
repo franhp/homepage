817f9179c92e19eacf4b4b0c6804c0be39e50642
Fran Hermoso <franhp@gmail.com>
2013-06-24 00:25:26 +0100

    and the about page

--- homepage/about/views.py
+++ homepage/about/views.py
@@ -1,1 +1,6 @@
-# Create your views here.
+from django.shortcuts import render_to_response
+from django.template.context import RequestContext
+
+
+def index(request):
+    return render_to_response('about.html', RequestContext(request, {}))
