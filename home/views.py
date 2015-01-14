e7dc2daf59f88c7f477a2572c32de7e2f1496893
Fran Hermoso <franhp@gmail.com>
2015-01-14 00:35:15 +0000

    Skeleton for the other apps

--- home/views.py
+++ home/views.py
@@ -10,15 +10,9 @@ from serializers import GithubProjectSerializer, TwitterSerializer, LastfmSerial
 class ContactView(TemplateView):
     template_name = 'contact.html'
 
-    def get_context_data(self, **kwargs):
-        return TemplateView.get_context_data(self, **kwargs)
-
 
 class HomeView(TemplateView):
     template_name = 'home.html'
-
-    def get_context_data(self, **kwargs):
-        return TemplateView.get_context_data(self, **kwargs)
 
 
 class GithubAPIView(APIView):
--- tvseries/views.py
+++ tvseries/views.py
@@ -1,13 +1,5 @@
-from django.shortcuts import render
 from django.views.generic.base import TemplateView
 
 
 class TVSeriesView(TemplateView):
-    '''
-    Test
-    '''
-    
     template_name = 'tvseries.html'
-    
-    def get_context_data(self, **kwargs):
-        return TemplateView.get_context_data(self, **kwargs)
