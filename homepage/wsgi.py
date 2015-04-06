5019592be058fa6886b1329a48482e7a1d599445
Fran Hermoso <franhp@gmail.com>
2015-04-06 22:12:45 +0100

    Added passenger wsgi

--- homepage/wsgi.py
+++ homepage/wsgi.py
@@ -7,6 +7,13 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 """
 
+import sys
+import os
+
+vendor_libs_path = os.path.join(os.getcwd(), 'lib')
+if os.path.exists(vendor_libs_path):
+    sys.path.insert(0, vendor_libs_path)
+
 import dotenv
 dotenv.read_dotenv()
 
---
+++ passenger_wsgi.py
@@ -0,0 +1,1 @@
+homepage/wsgi.py
