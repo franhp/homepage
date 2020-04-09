d45e4acdd3a4805a5118b1b0b2fc340809f7aba7
Fran Hermoso <franhp@gmail.com>
2020-04-09 20:39:10 +0200

    Remove testing and create first ranking class

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,34 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import books from './api/books.json';
+
+
+class Ranking extends React.Component {
+  renderItem(value) {
+    return (
+      <li className="RankingItem">{value.fields.name}</li>
+    );
+  }
+
+  render() {
+    return (
+      <div className="Ranking">
+        <h1>{this.props.title} ranking</h1>
+        <ol>
+          {this.props.content.map(book => { return this.renderItem(book) })}
+        </ol>
+      </div>
+    );
+  }
+}
 
 function App() {
   return (
     <div className="App">
+
       <header className="App-header">
+        <Ranking title="Books" content={books} />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -18,6 +41,7 @@ function App() {
         >
           Learn React
         </a>
+
       </header>
     </div>
   );
