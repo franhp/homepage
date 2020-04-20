653750e3ef5ac26274bea6abaa5f6aea8ae5d408
Fran Hermoso <franhp@gmail.com>
2020-04-21 00:53:28 +0200

    Wiki concept

--- frontend/src/modules/Wiki.js
+++ frontend/src/modules/Wiki.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { ListGroup, Container } from 'react-bootstrap';
+import { ListGroup, Tab, Accordion, Card } from 'react-bootstrap';
+import ReactMarkDown from 'react-markdown';
 import categories from '../api/wiki_categories.json';
 import documents from '../api/wiki.json';
 
@@ -8,17 +9,48 @@ import documents from '../api/wiki.json';
 class Wiki extends React.Component {
     render() {
         return (
-            <Container>
-                <ListGroup>
+            <Tab.Container>
+                <ListGroup horizontal>
                     {categories.map(category => {
                         return (
-                            <ListGroup.Item action>
+                            <ListGroup.Item action href={"#" + category.fields.slug}>
                                 {category.fields.name}
                             </ListGroup.Item>
                         )
                     })}
                 </ListGroup>
-            </Container>
+
+
+                <Tab.Content>
+                    {categories.map(category => {
+                        return (
+                            <Tab.Pane eventKey={"#" + category.fields.slug}>
+                                <Accordion>
+                                    {documents.map(document => {
+                                        if (document.fields.category === category.pk) {
+                                            return (
+                                                <Card>
+                                                    <Accordion.Toggle as={Card.Header} eventKey={document.fields.slug}>
+                                                        {document.fields.title}
+                                                    </Accordion.Toggle>
+                                                    <Accordion.Collapse eventKey={document.fields.slug}>
+                                                        <Card.Body>
+                                                            <ReactMarkDown source={document.fields.content} />
+                                                        </Card.Body>
+                                                    </Accordion.Collapse>
+                                                </Card>
+                                            )
+                                        } else {
+                                            return <></>
+                                        }
+                                    })}
+                                </Accordion>
+                            </Tab.Pane>
+                        )
+                    })}
+                </Tab.Content>
+
+            </Tab.Container>
 
         );
     }
