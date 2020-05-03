a45874cdc5c66024befaa7c199bf4957efe1e83e
Fran Hermoso <franhp@gmail.com>
2020-05-04 00:52:36 +0200

    Automate django migrations

--- refresh.sh
+++ refresh.sh
@@ -2,5 +2,6 @@
 
 docker-compose build
 docker-compose run --rm frontend npm run clean
+docker-compose run --rm backend python manage.py migrate
 docker-compose run --rm backend
 docker-compose run --rm frontend
