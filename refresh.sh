03e4309bec5ed9306a3f2518fe660a8699ff3cbe
Fran Hermoso <franhp@gmail.com>
2020-08-23 01:13:19 +0200

    Copy website to the new location

--- refresh.sh
+++ refresh.sh
@@ -5,3 +5,5 @@ docker-compose run --rm frontend npm run clean
 docker-compose run --rm backend python manage.py migrate
 docker-compose run --rm backend
 docker-compose run --rm frontend
+rm -rf /home/franhp/containers/nginx/webpages/homepage/*
+cp -r /home/franhp/containers/homepage/frontend/build/* /home/franhp/containers/nginx/webpages/homepage/
